96241dad22c38178daa4f5b7444e63120265968b
jamesawheel-stack <jamesawheel@gmail.com>
2026-02-13 02:39:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ async function igFetchJson(url) {
 
 async function getLatestMedia() {
   const url =
-    `https://graph.instagram.com/me/media` +
+    `https://graph.facebook.com/v24.0/${mediaId}` +
     `?fields=id,permalink,timestamp,caption` +
     `&limit=10` +
     `&access_token=${IG_ACCESS_TOKEN}`;
@@ -109,7 +109,7 @@ async function getLatestMedia() {
 
 async function getComments(mediaId) {
   let url =
-    `https://graph.instagram.com/${mediaId}/comments` +
+    `https://graph.facebook.com/v24.0/${mediaId}/comments` +
     `?fields=id,username,text,timestamp` +
     `&limit=50` +
     `&access_token=${IG_ACCESS_TOKEN}`;
